b194cf41a1bda46fff330895c6b22b2f7f814e03
rjatkinson2 <rjatkinson2@gmail.com>
2015-02-17 12:04:28 -0800

    Completed room and friend list functionality.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -16,11 +16,11 @@ app.init = function(){
   //   return app.init();
   // },1000);
   $("#refresh").on("click", function(){
-    console.log('test');
     app.fetch();
   });
 
   $("#form").on("submit",function(event){
+
     event.preventDefault();
     var text = $("#text").val();
     var name = $("#username").val();
@@ -28,12 +28,27 @@ app.init = function(){
     newMessage.username = name;
     newMessage.text = text;
     newMessage.roomname = "HR25";
-
     app.send(newMessage);
+    app.fetch();
   });
 
 
 
+};
+
+app.refreshEventHandlers = function(){
+  $("#rooms").change(function(){
+    var selectedRooms = [];
+    $("#rooms option:selected").each(function(){
+      selectedRooms.push(this.innerHTML);
+    });
+    // console.log(selectedRooms);
+    app.fetch(selectedRooms);
+  });
+
+  $("a").on("click", function(event){
+    event.preventDefault();
+  });
 };
 
 app.escape = function(data){
@@ -52,31 +67,59 @@ app.escape = function(data){
   });
 };
 
-app.refresh = function(data){
+app.refresh = function(data, callback){
   console.log(data);
   var chatBox = $('#chat');
   var allMessages = data.results;
   allMessages = app.escape(allMessages);
   var createMessage = function(message){
-    var innerHtml = "<div><div>username: " + message.username + "</div><div>text: "+ message.text + "</div><div>room: "+ message.roomname +"</div><div>createdAt: "+ message.createdAt +   "</div></div><br />";
+    var innerHtml = "<div><div>username: " +"<a class = 'username' href='#'>" + message.username + "</a>"+ "</div><div>text: "+ message.text + "</div><div>room: "+ message.roomname +"</div><div>createdAt: "+ message.createdAt +   "</div></div><br />";
     return innerHtml;
   };
+  var createMessageFriends = function(message){
+    var innerHtml = "<div><div>username: " +"<a class = 'username' href='#'>" + message.username + "</a>"+ "</div><div class = 'friend'>text: "+ message.text + "</div><div>room: "+ message.roomname +"</div><div>createdAt: "+ message.createdAt +   "</div></div><br />";
+    console.log("It works");
+    return innerHtml;
+  };
+
   chatBox.html('');
   for (var i = 0; i < allMessages.length; i++){
-    var innerHtml = createMessage(allMessages[i]);
+    if(_.contains(app.friends, allMessages[i].username)){
+      var innerHtml = createMessageFriends(allMessages[i]);
+    } else {
+      var innerHtml = createMessage(allMessages[i]);
+    }
     chatBox.append(innerHtml);
     app.rooms[allMessages[i].roomname] = 1;
   }
   app.createRooms();
+
+
+  $("#rooms").change(function(){
+    var selectedRooms = [];
+    $("#rooms option:selected").each(function(){
+      selectedRooms.push(this.innerHTML);
+    });
+    // console.log(selectedRooms);
+    app.fetch(selectedRooms);
+  });
+
+  $("a").on("click", function(event){
+    event.preventDefault();
+    app.friends.push(this.innerHTML);
+  });
+
 };
 
 app.rooms ={};
 app.createRooms = function(){
   for( key in this.rooms){
     // var option = $("<option>")
-    $("#rooms").append('<option> ' + key + '</option>');
+    $("#rooms").append('<option>' + key + '</option>');
   }
 };
+
+app.friends = [];
 
 app.send = function(message){
   $.ajax({
@@ -95,12 +138,18 @@ app.send = function(message){
   });
 };
 
-app.fetch = function(room){
-  // if(room !== undefined){
-    // var options ={order:"-createdAt", filter: "-room"};
-  // }else{
-    var options ={order:"-createdAt"};
-  // }
+app.fetch = function(room, users){
+  // var options ={order:"-createdAt", where:{"roomname":"lobby"}};
+  var options ={order:"-createdAt"};
+  if(room && users){
+    options.where = {"roomname":room, "username":{"$in":users}};
+  }
+  else if(room){
+    options.where = {"roomname":{"$in":room}};
+  }
+  else if (users){
+    options.where = {"username":{"$in":users}};
+  }
   $.ajax({
     url: app.server,
     type: 'GET',
